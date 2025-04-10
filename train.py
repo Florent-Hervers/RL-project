f306fffafd793e7c8fd304f964bce8b6562332d8
Florent-Hervers <flo230702@gmail.com>
2025-04-10 16:41:07 +0200

    Upgrade the train script to be easier to use

--- train.py
+++ train.py
@@ -7,106 +7,105 @@ import torch.nn as nn
 import wandb
 from wandb.integration.sb3 import WandbCallback
 import yaml
+from argparse import ArgumentParser, BooleanOptionalAction
+import os
 
-# Référence pour le dictionnaire d'hyperparamètre et des transformations:
-# https://huggingface.co/sb3/ppo_lstm-CarRacing-v0
+if __name__ == "__main__":
 
-"""
-DANS LE DICT IL Y A ENCORE
+    parser = ArgumentParser()
 
-https://rl-baselines3-zoo.readthedocs.io/en/master/guide/config.html
-https://stable-baselines3.readthedocs.io/en/master/common/logger.html => episodic len / episodic rew
+    parser.add_argument("-c", "--config", required=True, type=str, help="Filename of the configuration to use (without the .yaml extension)")
+    parser.add_argument("--config_path", default="configs/Q2", type=str, help="Relative path to the config directory.")
+    parser.add_argument("--wandb", "-w", required=True, type=str, help="Name of the run in wandb")
+    parser.add_argument("-o", "--output", default="trained_models/q2_final", type=str, help="Relative path + filename of the trained model")
+    parser.add_argument("--save_model", required=True, action=BooleanOptionalAction, help="This flag can be set to false using --no-save_model. If True, save the model in the file defined by the output argument.")
+    
+    args = parser.parse_args()
 
-('learning_rate', 'lin_1e-4') => decrease linéaire du lr je suppose (cfr TP5)
-('normalize', "{'norm_obs': False, 'norm_reward': True}") => Normalization des reward mais pas des observations
-('normalize_kwargs', {'norm_obs': False, 'norm_reward': False})] => à investiguer, c'est chelou car par cohérent avec le champ précédent
-"""
+    try:
+        with open(os.path.join(args.config_path, args.config + ".yaml"), "r") as f:
+            config = yaml.safe_load(f)
+    except Exception as e:
+        raise Exception(f"The following exceptions occured during the opening of the config file: {e.args}")
 
-# Charger la configuration YAML depuis "config.yaml"
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+    hyperparams = config["hyperparams"]
+    policy_kwargs = config["policy_kwargs"]
+    transformation = config["transformation"]
+    total_timesteps = config["total_timesteps"]
 
-# Extraire les hyperparamètres et la configuration de la policy
-hyperparams = config["hyperparams"]
-policy_kwargs = config["policy_kwargs"]
-transformation = config["transformation"]
-total_timesteps = config["total_timesteps"]
+    resize_obs_shape = tuple(transformation.get("resize_observation", None))
+    grayscale_params = transformation.get("grayscale_observation", None)
+    n_envs = transformation.get("n_envs", 1)
+    frame_stack = transformation.get("frame_stack", 1)
+    normalize_params = transformation.get("normalize", None)
 
-resize_obs_shape = tuple(transformation.get("resize_observation", None))
-grayscale_params = transformation.get("grayscale_observation", None)
-n_envs = transformation.get("n_envs", 1)
-frame_stack = transformation.get("frame_stack", 1)
-normalize_params = transformation.get("normalize", None)
+    # Si learning_rate est défini comme un dictionnaire dans le YAML,
+    # on crée une fonction de planification linéaire
+    if isinstance(hyperparams.get("learning_rate"), dict):
+        initial_lr = hyperparams["learning_rate"].get("initial", 1e-4)
+        def linear_schedule(progress_remaining):
+            return progress_remaining * initial_lr
+        hyperparams["learning_rate"] = linear_schedule
 
-# Si learning_rate est défini comme un dictionnaire dans le YAML,
-# on crée une fonction de planification linéaire
-if isinstance(hyperparams.get("learning_rate"), dict):
-    initial_lr = hyperparams["learning_rate"].get("initial", 1e-4)
-    def linear_schedule(progress_remaining):
-        return progress_remaining * initial_lr
-    hyperparams["learning_rate"] = linear_schedule
+    if "activation_fn" in policy_kwargs:
+        activation_str = policy_kwargs["activation_fn"]
+        policy_kwargs["activation_fn"] = getattr(nn, activation_str)
 
-# Convertir la chaîne de caractère de activation_fn en fonction réelle
-if "activation_fn" in policy_kwargs:
-    activation_str = policy_kwargs["activation_fn"]
-    policy_kwargs["activation_fn"] = getattr(nn, activation_str)
+    hyperparams["policy_kwargs"] = policy_kwargs
 
-# Ajouter policy_kwargs dans hyperparams pour le passer au modèle
-hyperparams["policy_kwargs"] = policy_kwargs
+    run = wandb.init(
+        entity="Rl2025-project",
+        project="RL Project",
+        name= args.wandb,
+        config=hyperparams,
+        sync_tensorboard=True,  
+        monitor_gym=True,       
+    )
 
-# Initialisation de wandb
-run = wandb.init(
-    entity="Rl2025-project",
-    project="RL Project",
-    name="First_full_test_with_yaml_config",
-    config=hyperparams,
-    sync_tensorboard=True,  # auto-upload des métriques tensorboard de sb3
-    monitor_gym=True,       # auto-upload des vidéos de l'agent
-    # save_code=True,       # optionnel
-)
+    def make_env():
+        def _init():
+            env = gym.make(
+                "CarRacing-v3",
+                continuous=True,
+                lap_complete_percent=0.95,
+                domain_randomize=False,
+                render_mode="rgb_array"
+            )
+            if resize_obs_shape is not None:
+                env = ResizeObservation(env, resize_obs_shape)
+            if grayscale_params is not None:
+                env = GrayscaleObservation(env, **grayscale_params)
+            env = Monitor(env)
+            return env
+        return _init
 
-# Fonction de création d'environnement
-def make_env():
-    def _init():
-        env = gym.make(
-            "CarRacing-v3",
-            continuous=True,
-            lap_complete_percent=0.95,
-            domain_randomize=False,
-            render_mode="rgb_array"
-        )
-        if resize_obs_shape is not None:
-            env = ResizeObservation(env, resize_obs_shape)
-        if grayscale_params is not None:
-            env = GrayscaleObservation(env, **grayscale_params)
-        env = Monitor(env)
-        return env
-    return _init
+    print("Loading CarRacing-v3 environment")
+    env = DummyVecEnv([make_env() for _ in range(n_envs)])
+    env = VecFrameStack(env, n_stack=frame_stack)
+    if normalize_params is not None:
+        env = VecNormalize(env, **normalize_params)
 
-print("Loading CarRacing-v3 environment")
-env = DummyVecEnv([make_env() for _ in range(n_envs)])
-env = VecFrameStack(env, n_stack=frame_stack)
-if normalize_params is not None:
-    env = VecNormalize(env, **normalize_params)
+    model = RecurrentPPO(
+        "CnnLstmPolicy",
+        env,
+        verbose=1,
+        stats_window_size=1,
+        tensorboard_log=f"runs/{run.id}",
+        **hyperparams
+    )
 
-print("Doing PPOLSTM")
-model = RecurrentPPO(
-    "CnnLstmPolicy",
-    env,
-    verbose=1,
-    stats_window_size=1,
-    tensorboard_log=f"runs/{run.id}",
-    **hyperparams
-)
+    model.learn(
+        total_timesteps=total_timesteps,
+        callback=WandbCallback(
+            verbose=2,
+        ),
+    )
 
-model.learn(
-    total_timesteps=total_timesteps,
-    callback=WandbCallback(
-        gradient_save_freq=100,
-        verbose=2,
-    ),
-)
+    wandb.finish()
 
-wandb.finish()
-# Sauvegarde du modèle final
-model.save("q2_final")
+    if args.save_model:
+        try:
+            model.save(args.output)
+        except Exception as e:
+            model.save("backup")
+            raise Exception(f"The following error occurs when saving the model: {e.args}")
