e1c11eb2752c03dfae63db42bc0fb8582202d485
Florent-Hervers <flo230702@gmail.com>
2025-04-27 12:24:43 +0200

    Add results for config 11 + update evaluation script (only config 6 and 11 were evaluated)

--- play_model.py
+++ play_model.py
@@ -1,14 +1,26 @@
+import time
+import yaml
 import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+from argparse import ArgumentParser, BooleanOptionalAction
 from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack, VecNormalize
 from gymnasium.wrappers import ResizeObservation, GrayscaleObservation
 
+
+parser = ArgumentParser()
+
+parser.add_argument("--human", default=True, action=BooleanOptionalAction, help="If true, display the run on the screen.")
+parser.add_argument("-c", "--config", type=int, required=True, help="The number of the config to evaluate")
+parser.add_argument("--evaluate", default=False, action=BooleanOptionalAction, help="If true, rerun the model 10 times to evaluate the model performances")
+
+args = parser.parse_args()
+
 def make_env(seed=None):
     def _init():
-        env = gym.make("CarRacing-v3", continuous=True, lap_complete_percent=0.95, domain_randomize=False, render_mode="rgb_array")
+        env = gym.make("CarRacing-v3", continuous=True, lap_complete_percent=0.95, domain_randomize=False, render_mode="rgb_array", max_episode_steps = 12000)
         env = ResizeObservation(env, (64, 64))
         env = GrayscaleObservation(env, keep_dim=True)
         if seed is not None:
@@ -17,17 +29,28 @@ def make_env(seed=None):
     return _init
 
 # Chargement du modèle
-model = RecurrentPPO.load("trained_models/q2_config6.zip")
+model = RecurrentPPO.load(f"trained_models/q2_config{args.config}.zip")
+
+try:
+    with open(f"configs/Q2/config{args.config}.yaml", "r") as f:
+        config = yaml.safe_load(f)
+except Exception as e:
+    raise Exception(f"The following exceptions occured during the opening of the config file: \n{e}")
 
 # Paramètres d'évaluation
-n_episodes = 10
+if args.evaluate:
+    n_episodes = 10
+    seed = 1
+else:
+    n_episodes = 1
+    seed = int(time.time())
 all_rewards = []
 
 for i in range(n_episodes):
-    print(f"\n=== Episode {i+1}/{n_episodes} ===")
-    env = DummyVecEnv([make_env(seed=i)])
-    env = VecFrameStack(env, n_stack=1)
-    env = VecNormalize(env, norm_reward=True, norm_obs=False)
+    if args.evaluate:
+        print(f"\n=== Episode {i+1}/{n_episodes} ===")
+    env = DummyVecEnv([make_env(seed=seed + i)])
+    env = VecFrameStack(env, n_stack=config["transformation"].get("frame_stack", 1))
     obs = env.reset()
 
     lstm_states = None
@@ -40,8 +63,8 @@ for i in range(n_episodes):
         obs, reward, done, info = env.step(action)
         episode_rewards.append(reward)
         episode_starts = done
-        # Commenter si pas besoin d'affichage
-        env.render("human")
+        if args.human:
+            env.render("human")
         done = done[0]
 
     total_reward = sum(episode_rewards)
@@ -52,12 +75,13 @@ for i in range(n_episodes):
 mean_reward = np.mean(all_rewards)
 std_reward = np.std(all_rewards)
 
-with open("Results/eval_results_config6.txt", "w") as f:
-    f.write("=== Results summary ===\n")
-    for i, reward in enumerate(all_rewards):
-        f.write(f"Track {i+1}: Mean reward = {reward.mean():.2f}\n")
-    f.write("\nSummary over all tracks:\n")
-    f.write(f"Mean reward: {mean_reward:.2f}\n")
-    f.write(f"Reward std: {std_reward:.2f}\n")
-    f.write(f"Min: {np.min(all_rewards):.2f}\n")
-    f.write(f"Max: {np.max(all_rewards):.2f}\n")
+if args.evaluate:
+    with open(f"Results/eval_results_config{args.config}.txt", "w") as f:
+        f.write("=== Results summary ===\n")
+        for i, reward in enumerate(all_rewards):
+            f.write(f"Track {i+1}: Mean reward = {reward.mean():.2f}\n")
+        f.write("\nSummary over all tracks:\n")
+        f.write(f"Mean reward: {mean_reward:.2f}\n")
+        f.write(f"Reward std: {std_reward:.2f}\n")
+        f.write(f"Min: {np.min(all_rewards):.2f}\n")
+        f.write(f"Max: {np.max(all_rewards):.2f}\n")
